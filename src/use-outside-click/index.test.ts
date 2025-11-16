77ced595b501850df82fa9df3cb61368a2755993
ludhiya-codaniel <242793836+ludhiya-codaniel@users.noreply.github.com>
2025-11-16 18:01:06 +0000

    Implement index.test.ts

---
+++ src/use-outside-click/index.test.ts
@@ -0,0 +1,48 @@
+import { renderHook } from '@testing-library/react';
+import { describe, it, expect, beforeEach, afterEach } from 'vitest';
+
+import { useOutsideClick } from '.';
+
+describe('useOutsideClick', () => {
+  let state = 0;
+  let container: HTMLDivElement;
+  let outOfContainer: HTMLDivElement;
+  const onClick = () => state++;
+
+  beforeEach(() => {
+    state = 0;
+    container = document.createElement('div');
+    outOfContainer = document.createElement('div');
+    document.body.appendChild(container);
+    document.body.appendChild(outOfContainer);
+  });
+
+  afterEach(() => {
+    document.body.removeChild(container);
+    document.body.removeChild(outOfContainer);
+  });
+
+  it('the state should be 0 when container click', () => {
+    const { rerender } = renderHook(() =>
+      useOutsideClick({ current: container }, onClick),
+    );
+
+    expect(state).toBe(0);
+
+    rerender();
+    container.click();
+    expect(state).toBe(0);
+  });
+
+  it('the state should be 1 when outside click', () => {
+    const { rerender } = renderHook(() =>
+      useOutsideClick({ current: container }, onClick),
+    );
+
+    expect(state).toBe(0);
+
+    rerender();
+    outOfContainer.click();
+    expect(state).toBe(1);
+  });
+});
